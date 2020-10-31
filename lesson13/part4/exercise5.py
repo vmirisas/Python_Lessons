83eeb996bdf9d79da44b59446cdbd37ec40868f3
Vasilis Mirisas <vmirisas@gmail.com>
2020-11-01 01:29:58 +0200

    Lesson 13 parts (3,4)

--- lesson13/part4/exercise5.py
+++ lesson13/part4/exercise5.py
@@ -2,12 +2,12 @@
 def selection_sort(array):
     for i in range(0, len(array)):
         pos = i
-        for j in range(i+1, len(array)):
+        for j in range(i + 1, len(array)):
             if array[j] < array[pos]:
                 pos = j
-        array[i],array[pos] = array[pos], array[i]
+        array[i], array[pos] = array[pos], array[i]
         print(array)
     return array
 
-print(selection_sort([5,4,3,1,2]))
 
+print(selection_sort([5, 4, 3, 1, 2]))
